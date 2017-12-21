f0971cd092b021a6a8719548276f6402d0bf4beb
Dasuni <dasunianupama@gmail.com>
2017-12-21 16:20:30 +0530

    modified ticket booking module

--- modules/tickets/server/models/ticket.server.model.js
+++ modules/tickets/server/models/ticket.server.model.js
@@ -12,8 +12,44 @@ var mongoose = require('mongoose'),
 var TicketSchema = new Schema({
   tclass: {
     type: String,
-    default: '',
+    default: 'first class',
     required: 'Please select Ticket class',
+    trim: true
+  },
+  refNo: {
+    type: Number,
+    default: '45632',
+    required: 'Please fill Ticket number',
+    trim: true
+  },
+  from: {
+    type: String,
+    default: 'Maradana',
+    required: 'Please select Start station',
+    trim: true
+  },
+  to: {
+    type: String,
+    default: 'Fort',
+    trim: true,
+    required: 'Please select End station'
+  },
+  ttype: {
+    type: String,
+    default: 'Single',
+    required: 'Please select Ticket type',
+    trim: true
+  },
+  price: {
+    type: Number,
+    default: '350.00',
+    required: 'Please fill Ticket price',
+    trim: true
+  },
+  tdate: {
+    type: String,
+    default: Date.now,
+    required: 'Please select Ticket date',
     trim: true
   },
   created: {
